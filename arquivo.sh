020431c9d522626b037f7dd5ca204aa84bc719d2
Pedro Furlan <159018663+PedroForleco@users.noreply.github.com>
2024-02-05 10:03:01 -0300

    Linux- Virtual Box
    Linhas de Programação para complementação do sistema Linux-Virtual Box
    Aula: SENAI- Celso Charuri
    Dia: 31/01/2024

---
+++ arquivo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Vamos iniciar as atividades"
+ sudo apt install update
+ sudo apt install upgrade 
+ sudo apt install phpmyadmim
+ sudo apt install php
+ sudo apt install mysql-server
+ sudo apt install apache2
+
+echo "Atividades Finalizadas"
+
